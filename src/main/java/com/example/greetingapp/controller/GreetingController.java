ee3f39924bc918654d307418fbea78634ab4f4ab
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-06 19:39:15 +0530

    Added: Code to find greeting message by Id in the Repository

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -18,30 +18,47 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     /**
-     * UC2 Extend reetingController to use Services Layer to get Simple Greeting message ”Hello World”
-     * http://localhost:8080/greeting
+     * UC2
+     * @Purpose: Extend reetingController to use Services Layer to get Simple Greeting message ”Hello World”
+     * @URL: http://localhost:8080/greeting
      * @Output: {
      *     "id": 1,
      *     "message": "Hello, world!"
      * }
      *
-     * UC3 Ability for the Greeting App to give Greeting message with
+     * UC3
+     * @Purpose: Ability for the Greeting App to give Greeting message with
      * 1. User First Name and Last Name or
      * 2. With just First Name or Last Name based on User attributes provides or
      * 3. Just Hello World.
-     * http://localhost:8080/greeting?name=rutuja&name=nandiwadekar
+     * @URL: http://localhost:8080/greeting?name=rutuja&name=nandiwadekar
      * @Output:{
      *     "id": 3,
      *     "message": "Hello, rutuja,nandiwadekar!"
      * }
      *
-     * UC4 Ability for the Greeting App to save the Greeting Message in the Repository
+     * UC4
+     * @Purpose: Ability for the Greeting App to save the Greeting Message in the Repository
      */
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name",defaultValue = "world") String name){
         User user = new User();
         user.setFirstName(name);
         return greetingService.addGreeting(user);
+    }
+
+    /**
+     * UC5
+     * @Purpose: Ability for the Greeting App to find a Greeting Message by Id in the Repository
+     * @URL: http://localhost:8080/path/3
+     * @Output: {
+     *     "id": 3,
+     *     "message": "Hello, world!"
+     * }
+     */
+    @GetMapping("/path/{id}")
+    public Greeting getElementById(@PathVariable Long id){
+        return greetingService.getGreetingById(id);
     }
 }
 
